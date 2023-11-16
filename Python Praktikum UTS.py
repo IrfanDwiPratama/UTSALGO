f46fd51031dca80e4014c001149b138efc5cfd9b
IrfanDwiPratama <145745268+IrfanDwiPratama@users.noreply.github.com>
2023-11-16 23:13:21 +0700

    Add files via upload

---
+++ Python Praktikum UTS.py
@@ -0,0 +1,13 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# Example usage:
+my_list = [50, 30, 40,10,20]
+insertion_sort(my_list)
+print("Sorted array:", my_list)
